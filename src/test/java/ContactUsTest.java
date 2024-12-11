85360a1a8802a878fe098dd61359353ad3c4fc7d
Haitham Alulaimi <93844383+HaithamAlulaimi@users.noreply.github.com>
2024-12-12 00:50:03 +0300

    AddComments
    AddComments

--- src/test/java/ContactUsTest.java
+++ src/test/java/ContactUsTest.java
@@ -1,6 +1,9 @@
+// Importing necessary helper classes and Selenium libraries for test execution.
+
 import helper.ActionsAssertion;
 import helper.RetryAnalyzer;
 import common.CommonMethods;
+import helper.WebDriverHelper;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -9,38 +12,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.ContactUsPage;
 
+/**
+ * This class contains automated test cases for verifying the Contact Us MasterTeam Web Page.
+ */
+
 public class ContactUsTest {
     WebDriver driver;
     ContactUsPage contactUsPage;
-    RetryAnalyzer.WebDriverHelper webDriverHelper;
+    WebDriverHelper webDriverHelper;
     SoftAssert softAssert;
     ActionsAssertion actionsAssertion;
 
-    @BeforeClass
-    public void beforeClass() {
-
-
-    }
 
     @BeforeMethod
     public void beforeMethod() {
-        driver = new ChromeDriver();
-        webDriverHelper = new RetryAnalyzer.WebDriverHelper(driver);
+        driver = new ChromeDriver(); // Initialize ChromeDriver.
+        webDriverHelper = new WebDriverHelper(driver);
         actionsAssertion = new ActionsAssertion(driver);
         contactUsPage = new ContactUsPage(driver);
         softAssert = new SoftAssert();
         CommonMethods.invokeBrowser(driver);
     }
 
+
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        driver.quit(); // Close the browser after the test.
     }
 
 
     @Test(description = "Fill the contact us with valid data.", priority = 1, retryAnalyzer = RetryAnalyzer.class)
     public void testContactUsFormSubmissionWithValidData() {
-
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Name"), "HaithamAlulaimi", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Email"), "Email@Email.com", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Phone"), "00962785247276", softAssert);
@@ -49,34 +51,39 @@ public class ContactUsTest {
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"Contact-Checkbox-02\"]"), softAssert);
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"wf-form-Contact-Form-3\"]/input"), softAssert);
 
-        Assert.assertEquals(webDriverHelper.findElementClassName("f-success-message").getText(), "Thank you! Your submission has been received!", "Message mismatch!");
-        Assert.assertTrue(webDriverHelper.findElementClassName("f-success-message").isDisplayed(), "The success message display is failed !!");
+        // Validating that the success message is displayed correctly.
+        Assert.assertEquals(webDriverHelper.findElementClassName("f-success-message").getText(),
+                "Thank you! Your submission has been received!", "Message mismatch!");
+        Assert.assertTrue(webDriverHelper.findElementClassName("f-success-message").isDisplayed(),
+                "The success message display has failed!");
         softAssert.assertAll();
     }
 
+
     @Test(description = "Fill the contact us with Invalid Email.", priority = 2)
     public void testContactUsFormSubmissionWithInValidEmail() {
-
         WebElement emailElement = webDriverHelper.findElementByID("Email");
 
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Name"), "HaithamAlulaimi", softAssert);
-        actionsAssertion.sendKeys(emailElement, "Email", softAssert);
+        actionsAssertion.sendKeys(emailElement, "Email", softAssert); // Sending invalid email format.
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Phone"), "00962785247276", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Contact-Message-Field-02"), "test", softAssert);
 
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"Contact-Checkbox-02\"]"), softAssert);
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"wf-form-Contact-Form-3\"]/input"), softAssert);
 
-        Assert.assertEquals(emailElement.getDomProperty("validationMessage"), "Please include an '@' in the email address. 'Email' is missing an '@'.", "Message mismatch!");
+        // Validating the browser's native validation message for invalid email.
+        Assert.assertEquals(emailElement.getDomProperty("validationMessage"),
+                "Please include an '@' in the email address. 'Email' is missing an '@'.", "Message mismatch!");
         softAssert.assertAll();
     }
 
+
     @Test(description = "Fill the contact us with Empty Name.", priority = 3)
     public void testContactUsFormSubmissionWithInValiName() {
-
         WebElement nameElement = webDriverHelper.findElementByID("Name");
 
-        actionsAssertion.sendKeys(nameElement, "", softAssert);
+        actionsAssertion.sendKeys(nameElement, "", softAssert); // Sending an empty name.
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Email"), "Email", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Phone"), "00962785247276", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Contact-Message-Field-02"), "test", softAssert);
@@ -84,44 +91,26 @@ public class ContactUsTest {
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"Contact-Checkbox-02\"]"), softAssert);
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"wf-form-Contact-Form-3\"]/input"), softAssert);
 
+        // Validating the browser's native validation message for empty name.
         Assert.assertEquals(nameElement.getDomProperty("validationMessage"), "Please fill out this field.", "Message mismatch!");
         softAssert.assertAll();
     }
 
+
     @Test(description = "Fill the contact us with Empty Email.", priority = 4)
     public void testContactUsFormSubmissionWithInEmptyEmail() {
-
         WebElement emailElement = webDriverHelper.findElementByID("Email");
 
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Name"), "HaithamAlulaimi", softAssert);
-        actionsAssertion.sendKeys(emailElement, "", softAssert);
+        actionsAssertion.sendKeys(emailElement, "", softAssert); // Sending an empty email.
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Phone"), "00962785247276", softAssert);
         actionsAssertion.sendKeys(webDriverHelper.findElementByID("Contact-Message-Field-02"), "test", softAssert);
 
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"Contact-Checkbox-02\"]"), softAssert);
         actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"wf-form-Contact-Form-3\"]/input"), softAssert);
 
+        // Validating the browser's native validation message for empty email.
         Assert.assertEquals(emailElement.getDomProperty("validationMessage"), "Please fill out this field.", "Message mismatch!");
         softAssert.assertAll();
     }
-
-    @Test(description = "Fill the contact us with Empty PhoneNumber.", priority = 5)
-    public void testContactUsFormSubmissionWithInEmptyPhoneNumber() {
-
-        WebElement PhoneNumber = webDriverHelper.findElementByID("Phone");
-
-        actionsAssertion.sendKeys(webDriverHelper.findElementByID("Name"), "HaithamAlulaimi", softAssert);
-        actionsAssertion.sendKeys(webDriverHelper.findElementByID("Email"), "Email@Email.com", softAssert);
-        actionsAssertion.sendKeys(webDriverHelper.findElementByID("Phone"), "", softAssert);
-        actionsAssertion.sendKeys(webDriverHelper.findElementByID("Contact-Message-Field-02"), "test", softAssert);
-
-        actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"Contact-Checkbox-02\"]"), softAssert);
-        actionsAssertion.click(webDriverHelper.findElementByXpath("//*[@id=\"wf-form-Contact-Form-3\"]/input"), softAssert);
-
-        Assert.assertEquals(PhoneNumber.getDomProperty("validationMessage"), "Please fill out this field.", "Message mismatch!");
-        softAssert.assertAll();
-    }
 }
-
-
-
